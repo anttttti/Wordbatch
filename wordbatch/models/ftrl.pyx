164a9b7b17cb08ddf4e684283b97d8e1c06dfc9f
anttip <antti.puurula@yahoo.com>
2016-09-26 18:53:29 +1300

    Small optimizations for FTRL

--- wordbatch/models/ftrl.pyx
+++ wordbatch/models/ftrl.pyx
@@ -17,7 +17,7 @@ cdef double inv_link_f(double e, int inv_link):
     return e
 
 cdef double predict_single(int* inds, double* vals, int lenn, double L1, double L2, double alpha, double beta,
-                double[:] w, double[:] z, double[:] n, int threads) nogil:
+                double[::1] w, double[::1] z, double[::1] n, int threads) nogil:
     cdef int i, ii
     cdef double sign, v
     cdef double e= 0.0
@@ -34,8 +34,8 @@ cdef double predict_single(int* inds, double* vals, int lenn, double L1, double 
         e += w[i] * v
     return e
 
-cdef void update_single(int* inds, double* vals, int lenn, double e, double alpha, double[:] w, double[:] z,
-                        double[:] n, int threads) nogil:
+cdef void update_single(int* inds, double* vals, int lenn, double e, double alpha, double[::1] w, double[::1] z,
+                        double[::1] n, int threads) nogil:
     cdef int i, ii
     cdef double g, g2, v
     for ii in prange(lenn+1, nogil=True, num_threads= threads):
@@ -51,9 +51,9 @@ cdef void update_single(int* inds, double* vals, int lenn, double e, double alph
         n[i] += g2
 
 cdef class FTRL:
-    cdef double[:] w
-    cdef double[:] z
-    cdef double[:] n
+    cdef double[::1] w
+    cdef double[::1] z
+    cdef double[::1] n
 
     cdef unsigned int threads
     cdef unsigned int iters
@@ -103,7 +103,7 @@ cdef class FTRL:
         #p= []
         p= np.zeros(X_indptr.shape[0]-1, dtype= np.float64)
         #cdef double[:] pp= p, pXvals= X.data, vals
-        cdef int i, lenn, row_count= X_indptr.shape[0]-1, row, ptr
+        cdef int lenn, row_count= X_indptr.shape[0]-1, row, ptr
 
         cdef int* inds2, indptr2
         cdef double* vals2
@@ -111,9 +111,9 @@ cdef class FTRL:
         for row in range(row_count):
             ptr= X_indptr[row]
             lenn= X_indptr[row + 1] - ptr
-            inds2= <int*> X_indices.data + ptr
-            vals2= <double*> X_data.data + ptr
-            p[row]= inv_link_f(predict_single(inds2, vals2, lenn, self.L1, self.L2, self.alpha, self.beta, self.w, self.z,
+            inds= <int*> X_indices.data + ptr
+            vals= <double*> X_data.data + ptr
+            p[row]= inv_link_f(predict_single(inds, vals, lenn, self.L1, self.L2, self.alpha, self.beta, self.w, self.z,
                                              self.n, threads), self.inv_link)
             #print row, pp[row], p.shape
         #print len(p)
@@ -131,20 +131,20 @@ cdef class FTRL:
                     np.ndarray[int, ndim=1, mode='c'] X_indices,
                     np.ndarray[int, ndim=1, mode='c'] X_indptr, y, int threads):
         cdef double alpha= self.alpha, beta= self.beta, L1= self.L1, L2= self.L2
-        cdef double[:] w= self.w, z= self.z, n= self.n, ys= y
-        cdef int lenn, i, ptr, row_count= X_indptr.shape[0]-1, row
+        cdef double[::1] w= self.w, z= self.z, n= self.n, ys= y
+        cdef int lenn, ptr, row_count= X_indptr.shape[0]-1, row
 
-        cdef int* inds2, indptr2
-        cdef double* vals2
+        cdef int* inds, indptr
+        cdef double* vals
 
         for iters in range(self.iters):
             for row in range(row_count):
                 ptr= X_indptr[row]
                 lenn= X_indptr[row+1]-ptr
-                inds2= <int*> X_indices.data+ptr
-                vals2= <double*> X_data.data+ptr
-                update_single(inds2, vals2, lenn,
-                              inv_link_f(predict_single(inds2, vals2, lenn, L1, L2, alpha, beta, w, z, n, threads),
+                inds= <int*> X_indices.data+ptr
+                vals= <double*> X_data.data+ptr
+                update_single(inds, vals, lenn,
+                              inv_link_f(predict_single(inds, vals, lenn, L1, L2, alpha, beta, w, z, n, threads),
                                        self.inv_link)-ys[row],
                               alpha, w, z, n, threads)
 
