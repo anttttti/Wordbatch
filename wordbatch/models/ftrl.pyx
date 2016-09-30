2765935c314ae02eac46794fbe0b34082086b37e
anttip <antti.puurula@yahoo.com>
2016-09-30 22:02:17 +1300

    FTRL optimizations

--- wordbatch/models/ftrl.pyx
+++ wordbatch/models/ftrl.pyx
@@ -17,7 +17,7 @@ cdef double inv_link_f(double e, int inv_link):
     return e
 
 cdef double predict_single(int* inds, double* vals, int lenn, double L1, double L2, double alpha, double beta,
-                double[::1] w, double[::1] z, double[::1] n, bint bias_term, int threads) nogil:
+                double[:] w, double[:] z, double[:] n, bint bias_term, int threads) nogil:
     cdef int i, ii, lenn2= lenn
     cdef double sign, v
     cdef double e= 0.0
@@ -30,14 +30,14 @@ cdef double predict_single(int* inds, double* vals, int lenn, double L1, double 
             i= 0
             v= 1.0
         sign = -1.0 if z[i] < 0 else 1.0
-        if sign * z[i] <= L1:  w[i] = 0.0
-        else:
+        if sign * z[i] > L1:
             w[i] = (sign * L1 - z[i]) / ((beta + sqrt(n[i])) / alpha + L2)
             e += w[i] * v
+        else:  w[i] = 0.0
     return e
 
-cdef void update_single(int* inds, double* vals, int lenn, double e, double alpha, double[::1] w, double[::1] z,
-                        double[::1] n, bint bias_term, int threads) nogil:
+cdef void update_single(int* inds, double* vals, int lenn, double e, double alpha, double[:] w, double[:] z,
+                        double[:] n, bint bias_term, int threads) nogil:
     cdef int i, ii, lenn2= lenn
     cdef double g, g2, v
     if bias_term:  lenn2+= 1
@@ -54,9 +54,10 @@ cdef void update_single(int* inds, double* vals, int lenn, double e, double alph
         n[i] += g2
 
 cdef class FTRL:
-    cdef double[::1] w
-    cdef double[::1] z
-    cdef double[::1] n
+    cdef double[:] w
+    cdef double[:] z
+    cdef double[:] n
+
     cdef unsigned int threads
     cdef unsigned int iters
     cdef unsigned int D
@@ -65,6 +66,7 @@ cdef class FTRL:
     cdef double alpha
     cdef double beta
     cdef int inv_link
+    cdef bint bias_term
 
     def __init__(self,
                  double alpha=0.1,
@@ -74,7 +76,9 @@ cdef class FTRL:
                  unsigned int D=2**25,
                  unsigned int iters=1,
                  int threads= 0,
-                 inv_link= "sigmoid"):
+                 inv_link= "sigmoid",
+                 bint bias_term=1):
+
         self.alpha= alpha
         self.beta= beta
         self.L1= L1
@@ -85,23 +89,26 @@ cdef class FTRL:
         self.threads= threads
         if inv_link=="sigmoid":  self.inv_link= 1
         if inv_link=="identity":  self.inv_link= 0
+        self.bias_term= bias_term
         self.w= np.zeros((self.D,), dtype=np.float64)
         self.z= np.zeros((self.D,), dtype=np.float64)
         self.n= np.zeros((self.D,), dtype=np.float64)
 
-    def predict(self, X, bint bias_term= True, int threads= 0):
+    def predict(self, X, int threads= 0):
         if threads==0:  threads= self.threads
         if type(X) != ssp.csr.csr_matrix:  X= ssp.csr_matrix(X, dtype=np.float64)
-# return self.predict_f(X, np.ascontiguousarray(X.data), np.ascontiguousarray(X.indices),
-#               np.ascontiguousarray(X.indptr), threads)
-        return self.predict_f(X.data, X.indices, X.indptr, bias_term, threads)
+        # return self.predict_f(X, np.ascontiguousarray(X.data), np.ascontiguousarray(X.indices),
+        #               np.ascontiguousarray(X.indptr), threads)
+        return self.predict_f(X.data, X.indices, X.indptr, threads)
 
     def predict_f(self, np.ndarray[double, ndim=1, mode='c'] X_data,
                     np.ndarray[int, ndim=1, mode='c'] X_indices,
-                    np.ndarray[int, ndim=1, mode='c'] X_indptr, bint bias_term, int threads):
+                    np.ndarray[int, ndim=1, mode='c'] X_indptr, int threads):
         p= np.zeros(X_indptr.shape[0]-1, dtype= np.float64)
+        cdef double[:] w= self.w, z= self.z, n= self.n
+        cdef double[:] pp= p
         cdef int lenn, row_count= X_indptr.shape[0]-1, row, ptr
-
+        cdef bint bias_term= self.bias_term
         cdef int* inds2, indptr2
         cdef double* vals2
         for row in range(row_count):
@@ -109,28 +116,27 @@ cdef class FTRL:
             lenn= X_indptr[row + 1] - ptr
             inds= <int*> X_indices.data + ptr
             vals= <double*> X_data.data + ptr
-            p[row]= inv_link_f(predict_single(inds, vals, lenn, self.L1, self.L2, self.alpha, self.beta, self.w, self.z,
-                                             self.n, bias_term, threads), self.inv_link)
+            pp[row]= inv_link_f(predict_single(inds, vals, lenn, self.L1, self.L2, self.alpha, self.beta, w, z, n,
+                                               bias_term, threads), self.inv_link)
         return p
 
-    def fit(self, X, y, bint bias_term= True, int threads= 0):
+    def fit(self, X, y, int threads= 0):
         if threads == 0:  threads= self.threads
         if type(X) != ssp.csr.csr_matrix:  X = ssp.csr_matrix(X, dtype=np.float64)
         if type(y) != np.array:  y = np.array(y, dtype=np.float64)
         # self.fit_f(X, np.ascontiguousarray(X.data), np.ascontiguousarray(X.indices),
         #           np.ascontiguousarray(X.indptr), y, threads)
-        self.fit_f(X.data, X.indices, X.indptr, y, threads, bias_term)
+        self.fit_f(X.data, X.indices, X.indptr, y, threads)
 
     def fit_f(self, np.ndarray[double, ndim=1, mode='c'] X_data,
                     np.ndarray[int, ndim=1, mode='c'] X_indices,
-                    np.ndarray[int, ndim=1, mode='c'] X_indptr, y, bint bias_term, int threads):
+                    np.ndarray[int, ndim=1, mode='c'] X_indptr, y, int threads):
         cdef double alpha= self.alpha, beta= self.beta, L1= self.L1, L2= self.L2
-        cdef double[::1] w= self.w, z= self.z, n= self.n, ys= y
+        cdef double[:] w= self.w, z= self.z, n= self.n, ys= y
         cdef int lenn, ptr, row_count= X_indptr.shape[0]-1, row
-
+        cdef bint bias_term= self.bias_term
         cdef int* inds, indptr
         cdef double* vals
-
         for iters in range(self.iters):
             for row in range(row_count):
                 ptr= X_indptr[row]
